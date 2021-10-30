230ae208bdc71e0214498c5e14fcdbf894ee7f14
anknnd <anknnd@outlook.com>
2021-10-30 10:18:40 +0530

    Part 3d validation exercises

--- part3/phonebook-backend/app/src/models/person.js
+++ part3/phonebook-backend/app/src/models/person.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
 const url = process.env.MONGODB_URI
 console.log('connecting to', url)
@@ -12,16 +13,31 @@ mongoose.connect(url)
     })
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String
+    name: {
+        type: String,
+        minLength: 3,
+        unique: true,
+        required: true
+    },
+    number: {
+        type: String,
+        validate: {
+            validator: function (v) {
+                return /^\d([- ]?\d){7,}$/gm.test(v);
+            },
+            message: props => `${props.value} is not a valid phone number. The phone number must have at least 8 digits separated by space or hyphen.`
+        },
+        required: true
+    }
 })
+personSchema.plugin(uniqueValidator);
 
 personSchema.set('toJSON', {
     transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
     }
-  })
+})
 
 module.exports = mongoose.model('Person', personSchema)
